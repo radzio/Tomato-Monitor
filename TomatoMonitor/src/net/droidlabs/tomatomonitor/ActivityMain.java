e0f0817724c55d0bfe771eaa09868d1e28cffa38
radoslaw.piekarz <radoslaw.piekarz@gmail.com>
2012-05-29 16:19:58 +0200

    Fixing travis

--- TomatoMonitor/src/net/droidlabs/tomatomonitor/ActivityMain.java
+++ TomatoMonitor/src/net/droidlabs/tomatomonitor/ActivityMain.java
@@ -7,7 +7,7 @@ public class ActivityMain extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bunadle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
