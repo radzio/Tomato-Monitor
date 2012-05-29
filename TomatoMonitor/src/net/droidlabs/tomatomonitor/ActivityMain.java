4dd6a9d1dd6aa983dcf192069256377c37c2d6a2
radoslaw.piekarz <radoslaw.piekarz@gmail.com>
2012-05-29 16:19:32 +0200

    Checking if travis is working.

--- TomatoMonitor/src/net/droidlabs/tomatomonitor/ActivityMain.java
+++ TomatoMonitor/src/net/droidlabs/tomatomonitor/ActivityMain.java
@@ -7,7 +7,7 @@ public class ActivityMain extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bunadle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
