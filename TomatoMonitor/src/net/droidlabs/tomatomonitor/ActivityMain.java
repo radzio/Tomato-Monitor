40c68c92226c6d46fcb6d362f9b954a151e3602e
Radek Piekarz <radziupiekarz@gmail.com>
2012-06-07 18:57:39 +0200

    Update.

--- TomatoMonitor/src/net/droidlabs/tomatomonitor/ActivityMain.java
+++ TomatoMonitor/src/net/droidlabs/tomatomonitor/ActivityMain.java
@@ -3,6 +3,26 @@ package net.droidlabs.tomatomonitor;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.text.Html;
+import android.util.*;
+import org.apache.http.*;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ActivityMain extends Activity
 {
     /** Called when the activity is first created. */
@@ -11,5 +31,58 @@ public class ActivityMain extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        new Thread()
+        {
+            public void run()
+            {
+                try {
+                    // URL url = new URL ("http://ip:port/download_url");
+                    URL url = new URL("http://10.89.9.1/update.cgi");
+                    String authStr = "";
+                    String authEncoded = Base64.encodeBytes(authStr.getBytes());
+                    Log.i("TEST", authEncoded);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Authorization", "Basic " + authEncoded);
+                    connection.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+                    String data = "exec=devlist&_http_id=TIDdf8a6ec71b1b7109";
+                    wr.write(data);
+                    wr.flush();
+
+
+                    InputStream in = (InputStream) connection.getInputStream();
+
+                    BufferedReader ina =
+                            new BufferedReader (new InputStreamReader (in));
+                    String line;
+                    while ((line = ina.readLine()) != null) {
+                        Log.i("TEST", line);
+                    }
+
+
+                    in.close();
+                }
+                catch (Exception e) {
+                    Log.e("TEST", "ERRRRRRRRRRRRRROR");
+                    e.printStackTrace();
+                }
+
+
+
+
+
+            }
+        }.start();
+
+
+
+
+
+
     }
+
+
+
 }
